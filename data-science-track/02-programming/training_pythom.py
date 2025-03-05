4728afb528485bc88a4bf12b35889387ea3c5fee
Konstantin Milonas <info@condata.io>
2025-03-06 00:27:36 +0100

    chore: updated notebooks

---
+++ data-science-track/02-programming/training_pythom.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("data-science-track\03-machine-learning\resources\exercise_regression_data.csv", sep=";", header=0, index_col=0)
